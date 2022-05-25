8ee3f92ff08f2f7160f0033606c9206ba6051f42
Vlad Danilov <vladislavs.danilovs@inbox.lv>
2022-05-25 15:26:34 +0300

    add save to db feature (frontend)

--- 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -1,9 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { CartItem } from 'src/app/common/cart-item';
 import { Country } from 'src/app/common/country';
+import { Order } from 'src/app/common/order';
+import { OrderItem } from 'src/app/common/order-item';
+import { Purchase } from 'src/app/common/purchase';
 import { State } from 'src/app/common/state';
 import { CartService } from 'src/app/services/cart.service';
+import { CheckoutService } from 'src/app/services/checkout.service';
 import { ShoppingMarketFormService } from 'src/app/services/shopping-market-form.service';
 import { ShoppingMarketValidators } from 'src/app/validators/shopping-market-validators';
 
@@ -32,7 +37,9 @@ export class CheckoutComponent implements OnInit {
 
   constructor(private formBuilder: FormBuilder,
               private cartService: CartService,
-              private shoppingMarketFormService: ShoppingMarketFormService) { }
+              private shoppingMarketFormService: ShoppingMarketFormService,
+              private checkoutService: CheckoutService,
+              private router: Router) { }
 
   ngOnInit(): void {
 
@@ -164,7 +171,6 @@ export class CheckoutComponent implements OnInit {
   }
 
   listCartDetails() {
-    this.cartItems = this.cartService.cartItems;
  
     this.cartService.totalPrice.subscribe(
       data => this.totalPrice = data
@@ -187,14 +193,93 @@ export class CheckoutComponent implements OnInit {
 
     if (this.checkoutFormGroup.invalid) {
       this.checkoutFormGroup.markAllAsTouched();
+      return;
       // Touching all fields triggers the display of the error messages
     }
+
+    // set up order
+    let order = new Order();
+    order.totalPrice = this.totalPrice;
+    order.totalQuantity = this.totalQuantity;
+
+    // get cart items
+    const cartItems = this.cartService.cartItems; 
+
+    // create orderItems for cartItems
+
+    /*-long way using for loop
+    let orderItems: OrderItem[] = [];
+    for (let i=0; i < cartItems.length; i++){
+      orderItems[i] = new OrderItem(cartItems[i]);
+    } */
+
+    //-short way of doing the same using functional programming
+    let orderItems: OrderItem[] = cartItems.map(tempCartItem => new OrderItem(tempCartItem));
+
+    // set up purchase
+    let purchase = new Purchase();
+
+    // populate purchase - customer
+    purchase.customer = this.checkoutFormGroup.controls['customer'].value;
+
+    // populate purchase - shipping address
+    purchase.shippingAddress = this.checkoutFormGroup.controls['shippingAddress'].value;
+
+    /*Get the state and country objects 
+    NOTE: JSON.parse(JSON.stringify(object)) this is simple one liner to deep clone an object, however this approach may cause data loss, but since we are not using: Dates, functions, undefined, Infinity, RegExps, Maps, Sets, Blobs, FileLists, 
+    ImageDatas, sparse Arrays, Typed Arrays or other complex types in our object this is not an issue*/
+    const shippingState: State = JSON.parse(JSON.stringify(purchase.shippingAddress.state));
+    const shippingCountry: Country = JSON.parse(JSON.stringify(purchase.shippingAddress.country));
+
+    purchase.shippingAddress.state = shippingState.name;
+    purchase.shippingAddress.country = shippingCountry.name;
+
+    // populate purchase - billing address
+    purchase.billingAddress = this.checkoutFormGroup.controls['billingAddress'].value;
+    const billingState: State = JSON.parse(JSON.stringify(purchase.billingAddress.state));
+    const billingCountry: Country = JSON.parse(JSON.stringify(purchase.billingAddress.country));
+    purchase.billingAddress.state = billingState.name;
+    purchase.billingAddress.country = billingCountry.name;
+
+    // populate purchase - order and orderItems
+    purchase.order = order;
+    purchase.orderItems = orderItems;
+
+    // call REST API via the CheckoutService
+    /* next: success/happe
+       error: error/ exception
+    */
+    this.checkoutService.placeOrder(purchase).subscribe({
+        next: response => {
+          alert(`Your order has been received.\nOrder tracking number: ${response.orderTrackingNumber}`);
+        
+          // reset cart
+          this.resetCart();
+        },
+        error: err => {
+          alert(`There was an error: ${err.message}`);
+        }
+      }
+    );
 
     console.log(this.checkoutFormGroup.get('customer')!.value);
     console.log("The email address is" + this.checkoutFormGroup.get('customer')!.value.email);
     console.log("The shipping address country is" + this.checkoutFormGroup.get('shippingAddres')!.value.country.name);
     console.log("The shipping address state is" + this.checkoutFormGroup.get('shippingAddres')!.value.state.name);
 
+  }
+
+  resetCart() {
+    // reset cart data
+    this.cartService.cartItems = [];
+    this.cartService.totalPrice.next(0);
+    this.cartService.totalQuantity.next(0);
+
+    // reset the form
+    this.checkoutFormGroup.reset();
+
+    // navigate back to the products page
+    this.router.navigateByUrl("/products");
   }
 
   handleMonthsAndYears() {
@@ -240,7 +325,7 @@ export class CheckoutComponent implements OnInit {
         }
 
         // select first item by default
-        formGroup?.get('state')?.setValue(data[0]);
+        formGroup.get('state').setValue(data[0]);
       }
     );
 
