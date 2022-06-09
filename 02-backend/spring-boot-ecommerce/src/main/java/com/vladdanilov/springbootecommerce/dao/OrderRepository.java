c7da23d0f954b9fb8866d2ca1d04ec5f0c1a37a3
Vlad Danilov <vladislavs.danilovs@inbox.lv>
2022-06-09 13:42:46 +0300

    add secure api for order history

--- 02-backend/spring-boot-ecommerce/src/main/java/com/vladdanilov/springbootecommerce/dao/OrderRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/vladdanilov/springbootecommerce/dao/OrderRepository.java
@@ -6,9 +6,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 @RepositoryRestResource
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
     Page<Order> findByCustomerEmailOrderByDateCreatedDesc(@Param("email") String email, Pageable pageable);
 
 }
--- 03-frontend/angular-ecommerce/src/app/app.module.ts
+++ 03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { ProductService } from './services/product.service';
 import { Router, RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
@@ -39,6 +39,7 @@ import myAppConfig from './config/my-app-config';
 import { MembersPageComponent } from './components/members-page/members-page.component';
 import { OrderHistoryComponent } from './components/order-history/order-history.component';
 import { Order } from './common/order';
+import { AuthInterceptorService } from './services/auth-interceptor.service';
 
 
 const oktaConfig = Object.assign({
@@ -96,7 +97,8 @@ const routes: Routes = [
     ReactiveFormsModule,
     OktaAuthModule
   ],
-  providers: [ProductService, {provide: OKTA_CONFIG, useValue: {oktaAuth} }],
+  providers: [ProductService, {provide: OKTA_CONFIG, useValue: {oktaAuth} },
+             {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
