7cd856a94f967a10bc05be76e52783dd43874f4a
Vlad Danilov <vladislavs.danilovs@inbox.lv>
2022-06-17 13:56:31 +0300

    add stripe details info for payment (email, owner etc.)

--- 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -46,6 +46,8 @@ export class CheckoutComponent implements OnInit {
   cardElement: any;
   displayError: any = "";
 
+  isDisabled: boolean = false;
+
   constructor(private formBuilder: FormBuilder,
               private cartService: CartService,
               private shoppingMarketFormService: ShoppingMarketFormService,
@@ -293,28 +295,45 @@ export class CheckoutComponent implements OnInit {
     purchase.orderItems = orderItems;
 
     // compute payment info
-    this.paymentInfo.amount = this.totalPrice * 100;
+    this.paymentInfo.amount = Math.round(this.totalPrice * 100);
     this.paymentInfo.currency = "EUR";
+    this.paymentInfo.receiptEmail = purchase.customer.email;
     
     // if valid form then
     // - create payment intent
     // - confirm card payment
     // - place order
 
+    console.log(`this.paymentInfo.amount: ${this.paymentInfo.amount}`);
+
     if(!this.checkoutFormGroup.invalid && this.displayError.textContent === "") {
       
+      this.isDisabled = true;
+
       this.checkoutService.createPaymentIntent(this.paymentInfo).subscribe(
         (paymentIntentResponse) => {
           this.stripe.confirmCardPayment(paymentIntentResponse.client_secret,
             {
               payment_method: {
-                card: this.cardElement
+                card: this.cardElement,
+                billing_details: {
+                  email: purchase.customer.email,
+                  name: `${purchase.customer.firstName} ${purchase.customer.lastName}`,
+                  address: {
+                    line1: purchase.billingAddress.street,
+                    city: purchase.billingAddress.city,
+                    state: purchase.billingAddress.state,
+                    postal_code: purchase.billingAddress.zipCode,
+                    country: this.billingAddressCountry.value.code
+                  }
+                }
               }
             }, { handleActions: false})
             .then(function(result) {
               if (result.error) {
                 // inform the customer there was an error
                 alert(` There was an error: ${result.error.message}`);
+                this.isDisabled = false;
               } else {
                 // call REST API via the CheckoutService
                 this.checkoutService.placeOrder(purchase).subscribe({
@@ -323,6 +342,7 @@ export class CheckoutComponent implements OnInit {
                   
                     // reset cart
                     this.resetCart();
+                    this.isDisabled = false;
                   },
                   error: err => {
                     alert(`There was en error: ${err.message}`);
@@ -373,10 +393,11 @@ export class CheckoutComponent implements OnInit {
     this.checkoutFormGroup.reset();
 
     // clear the cart contents from the localstorage
-    localStorage.clear();
+    sessionStorage.clear();
 
     // navigate back to the products page
     this.router.navigateByUrl("/products");
+
   }
 
   handleMonthsAndYears() {
